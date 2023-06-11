9ec2f98f7b302da2a459d52c926fc48cf119b17b
4alf <kingalifa11@gmail.com>
2023-06-11 09:53:25 +0100

    create componests and subComponents

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./globalStyles";
+
+
+import Main from "./components/Main";
+//import AboutPage from "./components/AboutPage";
+
+
+
+function App() {
+  const location = useLocation();
+  return (
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+       
+
+       
+        <AnimatePresence mode='wait'>
+          
+          <Routes key={location.pathname} location={location} >
+            
+
+            <Route path="/" element={<Main />} />
+
+            <Route path="*" element={<Main />} />
+          </Routes>
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,165 @@
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+import LogoComponent from '../subComponents/LogoComponent';
+import { Endless } from './AllSvgs';
+import Intro from './Intro';
+
+
+const MainContainer = styled.div`
+background: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+overflow:hidden;
+
+position: relative;
+
+h2,h3,h4,h5,h6{
+  font-family:'Karla', sans-serif ;
+  font-weight:500;
+}
+`
+
+const Container = styled.div`
+padding: 2rem;
+`
+
+const Contact = styled.a`
+color: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+right: calc(1rem + 2vw);
+text-decoration: none;
+z-index:1;
+`
+
+const BottomBar = styled.div`
+position: absolute;
+bottom: 1rem;
+left: 0;
+right: 0;
+width: 100%;
+
+display: flex;
+justify-content: space-evenly;
+`
+const ABOUT = styled(NavLink)`
+color: ${props => props.click ? props.theme.body : props.theme.text};
+text-decoration: none;
+z-index:1;
+`
+
+
+const rotate = keyframes`
+from{
+    transform: rotate(0);
+}
+to{
+    transform: rotate(360deg);
+}
+`
+
+const Center = styled.button`
+position: absolute;
+top: ${props => props.click ? '85%' :'50%'  };
+left: ${props => props.click ? '92%' :'50%'  };
+transform: translate(-50%,-50%);
+border: none;
+outline: none;
+background-color: transparent;
+cursor: pointer;
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+transition: all 1s ease;
+
+&>:first-child{
+    animation: ${rotate} infinite 1.5s linear;
+}
+
+&>:last-child{
+    display: ${props => props.click ? 'none' :'inline-block'  };
+    padding-top: 1rem;
+}
+`
+
+const DarkDiv = styled.div`
+position: absolute;
+top: 0;
+background-color: #000;
+bottom: 0;
+right: 50%;
+width: ${props => props.click ? '50%' : '0%'};
+height: ${props => props.click ? '100%' : '0%'};
+z-index:1;
+transition: height 0.5s ease, width 1s ease 0.5s;
+`
+
+
+const Main = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <MainContainer>
+         <DarkDiv   click={click}/>
+            <Container>
+           
+            <LogoComponent theme={click ? 'dark' :'light'}/>
+            <Center click={click}>
+                <Endless  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
+                <span>Tap</span>
+            </Center>
+
+            <Contact target="_blank" href="mailto:codebucks27@gmail.com">
+                <motion.h2
+                initial={{
+                    y:-200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                
+                >
+                    Say hi..
+                </motion.h2>
+            </Contact>
+           
+            
+            <BottomBar>
+            <ABOUT to="/about" click={+click}>
+                <motion.h2
+                initial={{
+                    y:200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                 whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                >
+                    About.
+                </motion.h2>
+            </ABOUT>
+           
+
+            </BottomBar>
+
+            </Container>
+            {click ? <Intro click={click} /> : null }
+        </MainContainer>
+    )
+}
+
+export default Main
