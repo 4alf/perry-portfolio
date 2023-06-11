01643d5ed9bc5293c6e1650464fa4b908ea36660
4alf <kingalifa11@gmail.com>
2023-06-11 14:18:17 +0100

    create LogoComponents

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
-import { DarkTheme } from '../components/Themes'
+//import { DarkTheme } from '../components/Themes'
 
 
 
 
 const Logo = styled.h1`
 display: inline-block;
-color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
-font-family: 'Pacifico',cursive;
+color: #000;
+
+font-family: system-ui;
 
 position: fixed;
 left: 2rem;
 top: 2rem;
 z-index:3;
 `
-
+// {${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};}
 const LogoComponent = (props) => {
     return (
         <Logo color={props.theme}>
