32fa40df3be1f1c82fdcbe420e1d7702d43e3040
4alf <kingalifa11@gmail.com>
2023-06-12 17:05:37 +0100

    write skelton

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -9,3 +9,28 @@ import ParticleComponent from '../subComponents/ParticleComponent';
 import BigTitle from '../subComponents/BigTitlte'
 import astronaut from '../assets/Images/spaceman.png'
 
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+            <Box>
+                <LogoComponent theme="dark" />
+                <SocialIcons theme ="dark" />
+                <ParticleComponent theme="dark" />
+
+                <ArmStrong>
+                    <img src ={hoverman} alt="nasaman" />
+                </ArmStrong>
+
+                <Main>
+                    who you areeeeeee!!! <br></br> Perry Amos <br></br>
+                </Main>
+
+                <BigTitle text="ABOUT" top="10%" left="5%" />
+            </Box>
+        </ThemeProvider>
+    )
+}
+
+export default AboutPage
