547f41f42d793ddf775855bc58cabdb78bacb403
4alf <kingalifa11@gmail.com>
2023-06-12 16:49:31 +0100

    🏗 about page

---
+++ src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
+
