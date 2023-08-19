a4bf5573d5bbf02d437fd8ce6966042112ff65c7
4alf <kingalifa11@gmail.com>
2023-08-19 18:56:27 +0100

    add about

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import styled, { keyframes, ThemeProvider } from 'styled-components'
+import React from 'react';
+import styled, { keyframes, ThemeProvider } from 'styled-components';
 import {DarkTheme} from './Themes';
 
 import HomeButton from '../subComponents/HomeButton';
 import LogoComponent from '../subComponents/LogoComponent';
 import SocialIcons from '../subComponents/SocialIcons';
 //import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTits from '../subComponents/BigTits'
-import hoverman from '../assets/Images/PerryAmos.png'
-
+import BigTits from '../subComponents/BigTits';
+import hoverman from '../assets/Images/PerryAmos.png';
 const Box = styled.div`
 background-color: ${props => props.theme.body};
 width:100vw;
@@ -69,7 +68,7 @@ const AboutPage = () => {
                 </ArmStrong>
 
                 <Main>
-                    Ia m a UI/UX designer who is very passionate about creating visually appealing design that are both beautiful and functional. 
+                    I'm a UI/UX designer who is very passionate about creating visually appealing design that are both beautiful and functional. 
                     My work is centered around the user's needs and preferences, and i take pride in designing interfaces that enhance their interaction with digital 
                     product seamless. i am constantly exploring and learning about the latest trends, techniques and technology in the field of UI/UX design to stay ahead
                     of the curve. I belive that design is an iterative process, and i am always looking for ways to improve my craft and create better design.
