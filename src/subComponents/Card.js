5078dd17f5636983e89f8cf3eb0b1473f2bbb6a8
4alf <kingalifa11@gmail.com>
2023-08-28 18:44:08 +0100

    create cards

---
+++ src/subComponents/Card.js
@@ -0,0 +1,53 @@
+import { motion } from 'framer-motion';
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Github } from '../components/AllSvgs';
+
+
+
+
+// Framer motion configuration
+const Item = {
+    hidden:{
+        scale:0
+    },
+    show:{
+        scale:1,
+        transition: {
+            type: 'spring',
+            duration: 0.5
+        }
+    }
+}
+
+const Card = (props) => {
+
+    const {id, name, description, tags, demo, github} = props.data;
+
+    return (
+        <Box key={id} variants={Item}>
+            <Title>{name}</Title>
+            <Description>
+                {description}
+            </Description>
+            <Tags>
+            {
+                    tags.map((t,id) => {
+                        return <Tag key={id}>#{t}</Tag>
+                    })
+                }
+            </Tags>
+            <Footer>
+                <Link href={demo} target="_blank">
+                    Visit
+                </Link>
+                <Git  href={github}  target="_blank">
+                    <Github width={30} height={30} />
+                </Git>
+            </Footer>
+        </Box>
+    )
+}
+
+export default Card
