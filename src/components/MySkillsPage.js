4c51f9b4dc4c72826df42cd737d34c5fc1bdad48
4alf <kingalifa11@gmail.com>
2023-08-28 18:49:46 +0100

    styled component

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -10,7 +10,77 @@ import PowerButton from '../subComponents/PowerButton';
 import ParticleComponent from '../subComponents/ParticleComponent';
 import BigTitle from '../subComponents/BigTitlte'
 
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
 
+
+`
+
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index:3;
+line-height: 1.5;
+cursor: pointer;
+
+font-family: 'Ubuntu Mono',monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+&:hover{
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+
+${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body};
+    }
+}
+
+&>*:first-child{
+margin-right: 1rem;
+}
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+
+
+${Main}:hover &{
+   
+        color:${props => props.theme.body};
+    
+}
+
+strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+}
+ul,p{
+    margin-left: 2rem;
+}
+`
 
 const MySkillsPage = () => {
     return (
